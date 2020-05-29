ec456e291dfb68531f7fce7e5f61b71aa61c1714
el-gringo-alto <22480930+el-gringo-alto@users.noreply.github.com>
2020-05-29 18:13:08 -0400

    added files

---
+++ commands/check.js
@@ -0,0 +1,33 @@
+module.exports = {
+    name: 'check',
+    aliases: ['c'],
+    description: 'Roll a d20. You can add a modifier after the command call to add it to the result.',
+    usage: '<modifier>',
+    execute(message, args) {
+        let modifier;
+
+        // convert the argument to a number or make it 0 if not given
+        if (!args[0]) {
+            modifier = 0;
+        } else {
+            modifier = Number(args[0]);
+        }
+
+        // reply with an error if the argument given is not a number
+        if (isNaN(modifier)) {
+            message.reply('modifer needs to be a number');
+            return;
+        }
+
+        // dice roll
+        let roll = Math.floor(Math.random() * 20) + 1;
+
+        msg = `**${roll + modifier}**`
+
+        if (modifier != 0) {
+            msg += ` (${roll} + ${modifier})`
+        }
+
+        message.reply(msg);
+    },
+};
---
+++ commands/roll.js
@@ -0,0 +1,63 @@
+module.exports = {
+    name: 'roll',
+    description: 'Roll a die.',
+    aliases: ['r'],
+    args: true,
+    usage: '<number of dice>d<sides of dice><+/- modifier>',
+    execute(message, args) {
+
+        diceRegex = /(\d+)?d(\d+)((?:\+|\-)\d+)?/i;
+
+        let msg;
+
+        // chaining dice rolls
+        for (var arg of args) {
+            let die = arg.match(diceRegex);
+
+            if (die == null) {
+                message.reply(`**${die[0]}** is not a valid dice roll`);
+                continue;
+            }
+
+            msg = `**${die[0]}**\n`
+
+            // number of dice
+            // if not given, this will be 1
+            let num_dice = 1;
+            if (die[1] != null) {
+                num_dice = die[1]
+            };
+
+            // type of dice
+            let dice_type = die[2];
+
+            // modifier
+            // if not given, this will be 0
+            let modifier = 0;
+            if (die[3] != null) {
+                modifier = Number(die[3])
+            };
+
+            let rolls = [];
+
+            for (var i = 0; i < num_dice; i++) {
+                let dice_roll = Math.floor(Math.random() * dice_type) + 1;
+                msg += `> Die ${i + 1}: ${dice_roll}\n`;
+                rolls[i] = dice_roll;
+            }
+
+            let sumOfRolls = 0;
+            for (var dice_roll of rolls) {
+                sumOfRolls += dice_roll
+            };
+
+            msg += `> Total: **${sumOfRolls + modifier}**`
+            if (modifier != 0) {
+                msg += ` (${sumOfRolls} + ${modifier})`
+            }
+            message.reply(msg)
+        }
+
+
+    },
+};
